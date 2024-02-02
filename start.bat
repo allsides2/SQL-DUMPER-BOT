7dc83a49a2605380f3e603231ad8d8422fa78fdf
allsides <juniorespindola201@gmail.com>
2024-02-02 20:57:15 +0000

    first commit

---
+++ start.bat
@@ -0,0 +1,22 @@
+@echo off
+color 0c
+echo -
+echo [BOT-DUMPER-BY-JS] Deletando cache ...
+echo -
+rd /s /q "./cache"
+timeout 2
+test&cls
+color 02  
+
+echo .
+echo .
+echo .
+
+echo [BOT-DUMPER-BY-JS] Bot desenvolvido por </Jussen>#7371 ...
+
+echo .
+echo .
+echo .
+echo .
+
+npx nodemon
